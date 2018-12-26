e1b0e201af1a3f1ad63a8fef6cba10ad0dc2bab5
jiangxq <1083873272@qq.com>
2018-12-26 13:59:59 +0800

    [dev] update build.gradle

--- film_dev/lib/main.dart
+++ film_dev/lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'FilmDev',
         theme: new ThemeData(
           brightness: Brightness.dark,
           primaryColor: Colors.black,
